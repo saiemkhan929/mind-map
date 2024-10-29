9d4361f3da1c54287f620914c51b81742b0a58c3
saiemkhan929 <saiemkhan929@gmail.com>
2024-10-29 01:05:52 +0100

    project initialized

---
+++ mind-map/src/test/java/com/saiemkhan/mindmap/MindMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saiemkhan.mindmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
